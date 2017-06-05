b99a94ff0a2bdf0800fdffb813839f7261cf6e4b
Francisco Peters <francisco.peters91@gmail.com>
2017-06-04 20:31:21 -0400

    fixes

--- detphToPCD.m
+++ detphToPCD.m
@@ -1,6 +1,5 @@
 depth = imread('00000-depth.png');
 [pcloud, distance] = depthToCloud(depth);
-pcloud(200, 200,:);
 fid = fopen('result.pcd','wt');
 fprintf(fid, '# .PCD v.7 - Point Cloud Data file format\nVERSION .7\nFIELDS x y z rgb\nSIZE 4 4 4 4\nTYPE F F F F\nCOUNT 1 1 1 1\nWIDTH 640\nHEIGHT 480\nVIEWPOINT 0 0 0 1 0 0 0\nPOINTS 307200\nDATA ascii\n');
 for i=1:640
